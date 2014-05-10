a6516bd084b973eb3c07f8307d8eca53d92526da
Nyholm <tobias.nyholm@gmail.com>
2014-05-10 17:55:14 +0200

    init

---
+++ HappyRExcerptBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HappyR\ExcerptBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class HappyRExcerptBundle
+ *
+ * @author Tobias Nyholm
+ *
+ *
+ */
+class HappyRExcerptBundle extends Bundle
+{
+}
